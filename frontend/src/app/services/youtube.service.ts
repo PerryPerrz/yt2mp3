7edfbc6b9ce449a34653ce4015251b910823a5df
PerryPerrz <iophugstud6@gmail.com>
2026-04-19 17:09:59 +0200

    Fix api url for production

--- frontend/src/app/services/youtube.service.ts
+++ frontend/src/app/services/youtube.service.ts
@@ -1,13 +1,15 @@
+// frontend/src/app/services/youtube.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { VideoInfo } from '../models/video-info.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class YoutubeService {
-  private apiUrl = 'http://localhost:3000/api';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
